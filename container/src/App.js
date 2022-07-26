4b30d0ebaca08057fb779bdb84feef49fbd9f303
Phat Nguyen <thienphat1205@gmail.com>
2022-07-27 00:08:59 +0700

    add react router v6 ⚡️

--- container/src/App.js
+++ container/src/App.js
@@ -1,34 +1,31 @@
-import React, { lazy, Suspense, useState } from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import {
-  StylesProvider,
-  createGenerateClassName,
-} from "@material-ui/core/styles";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 const KtcLcApp = lazy(() => import("./components/KtcLcApp"));
 const QlclApp = lazy(() => import("./components/QlclApp"));
 
-const generateClassName = createGenerateClassName({
-  productionPrefix: "co",
-});
-
 export default () => {
-  const [isSignedIn, setIsSignedIn] = useState(false);
   return (
     <BrowserRouter>
-      <StylesProvider generateClassName={generateClassName}>
-        <div>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Switch>
-              <Route exact path="/">
-                <Redirect to="/ktc-lc" />
-              </Route>
-              <Route path="/ktc-lc" component={KtcLcApp} />
-              <Route path="/qlcl" component={QlclApp} />
-            </Switch>
-          </Suspense>
-        </div>
-      </StylesProvider>
+      <Routes>
+        <Route exact path="/" element={<Navigate to="/ktc-lc" />} />
+        <Route
+          path="/ktc-lc/*"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <KtcLcApp />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/qlcl/*"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <QlclApp />
+            </Suspense>
+          }
+        />
+      </Routes>
     </BrowserRouter>
   );
 };
