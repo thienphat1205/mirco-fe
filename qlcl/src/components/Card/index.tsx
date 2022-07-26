37a7b8e9f508d55758f204a65ae599755a9ae514
Phat Nguyen <thienphat1205@gmail.com>
2022-07-26 17:33:18 +0700

    no message

--- qlcl/src/components/Card/index.tsx
+++ qlcl/src/components/Card/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./index.module.less";
 
-const Card: React.FC = ({ children }) => {
+const Card: React.FC<{ children: any }> = ({ children }) => {
   return (
     <div className={styles.card} style={{ minHeight: "calc(100vh - 80px)" }}>
       {children}
